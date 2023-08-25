7c5bfcd7d93487f2eeb0c96baabb2422ab9fec28
IMP_Lab <j218305@ns.kogakuin.ac.jp>
2023-08-25 14:06:43 +0900

    modify readme file

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Youtube Link: https://www.youtube.com/watch?v=2iS5rQlTTB4
 
+<img src="Image/login.png" alt="Login Page" width="300" />
+<img src="Image/Signup.png" alt="SignUp Page" width="300" />
+<img src="Image/main.png" alt="Main Page" width="300" />
--- app/src/main/java/com/tutorial/tutorialno81/MainActivity.kt
+++ app/src/main/java/com/tutorial/tutorialno81/MainActivity.kt
@@ -15,17 +15,18 @@ import com.tutorial.tutorialno81.ui.theme.TutorialNo81Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TutorialNo81Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+//        setContent {
+//            TutorialNo81Theme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+        setContentView(R.layout.activity_main)
     }
 }
 
